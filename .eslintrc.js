1b099f4ce359f13bcc3f2460aa926c1d4c0db9bc
donkeeman <doonkeeemaan@gmail.com>
2022-09-28 00:18:20 +0900

    Setting: ESLint, TypeScript 설정 수정 및 기본 파일 삭제

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		"plugin:vue/essential",
+		"@vue/standard",
+		"@vue/typescript/recommended",
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		quotes: [2, "double", { avoidEscape: false }],
+		semi: 0,
+		"comma-dangle": 0,
+		indent: [2, "tab"],
+		"no-tabs": 0,
+	},
+};
