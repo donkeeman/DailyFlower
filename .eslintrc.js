2d0eb61cb9cea25be98f52a9c0d7e499d8b9074e
donkeeman <doonkeeemaan@gmail.com>
2022-09-28 13:29:42 +0900

    Setting: ESLint 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true,
-	},
-	extends: [
-		"plugin:vue/essential",
-		"@vue/standard",
-		"@vue/typescript/recommended",
-	],
-	parserOptions: {
-		ecmaVersion: 2020,
-	},
-	rules: {
-		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-		quotes: [2, "double", { avoidEscape: false }],
-		semi: 0,
-		"comma-dangle": 0,
-		indent: [2, "tab"],
-		"no-tabs": 0,
-	},
-};
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        "plugin:vue/essential",
+        "@vue/standard",
+        "@vue/typescript/recommended",
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        quotes: [2, "double", { avoidEscape: false }],
+        semi: 0,
+        "comma-dangle": 0,
+        indent: "off",
+        "no-tabs": 0,
+        "space-before-function-paren": ["error", "never"],
+    },
+}
