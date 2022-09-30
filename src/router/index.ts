623a25beb5d7182511c476795a68ce160dd4d338
donkeeman <doonkeeemaan@gmail.com>
2022-09-30 16:06:56 +0900

    Feat: 라우터 설정 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,21 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
+import TodayFlower from "@/components/TodayFlower.vue";
+import FlowerSearch from "@/components/FlowerSearch.vue";
+import FlowerInfo from "@/components/FlowerInfo.vue";
 
 Vue.use(VueRouter);
 
-const routes: Array<RouteConfig> = [];
+const routes: Array<RouteConfig> = [
+    { path: "/", component: TodayFlower },
+    { path: "/search", component: FlowerSearch },
+    { path: "/info/:index", component: FlowerInfo },
+];
 
 const router = new VueRouter({
-	mode: "history",
-	base: process.env.BASE_URL,
-	routes,
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
 });
 
 export default router;
