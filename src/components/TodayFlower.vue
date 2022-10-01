eca93dac9b82b90a7738110616ef38ed8c3d26f6
donkeeman <doonkeeemaan@gmail.com>
2022-10-02 03:35:14 +0900

    Fix: flowerData에 더 많은 정보 저장하도록 수정 및 프로퍼티 이름 변경

--- src/components/TodayFlower.vue
+++ src/components/TodayFlower.vue
@@ -1,22 +1,20 @@
 <template>
-    <div class="todayContainer">
+    <article class="todayContainer">
         <!-- v-if로 데이터를 다 불러오면 띄우기 -->
         <!-- api 에러났을 경우에는 잠시후 다시 시도해 주세요 띄우기 -->
         <!-- 데이터 불러왔는지 체크하는 변수와 데이터가 에러인지 체크하는 변수 추가하기 -->
         <h2>오늘의 꽃</h2>
         <div class="imgWrapper">
-            <img :src="flowerData.flowerImg[0]" alt="꽃 사진" />
+            <img :src="flowerData.imgArray[0]" alt="꽃 사진" />
         </div>
-        <strong class="flowerName" title="꽃 이름">{{
-            flowerData.flowerName
-        }}</strong>
-        <p class="flowerLanguage" title="꽃말">
-            " {{ flowerData.flowerLanguage }} "
-        </p>
-        <router-link :to="'/info/' + today.dateNo" class="infoLink"
-            >자세히 알아보기</router-link
-        >
-    </div>
+        <strong class="flowerName" title="꽃 이름">
+            {{ flowerData.korName }}
+        </strong>
+        <p class="flowerLanguage" title="꽃말">" {{ flowerData.language }} "</p>
+        <router-link :to="'/info/' + today.dateNo" class="infoLink">
+            자세히 알아보기
+        </router-link>
+    </article>
 </template>
 
 <script lang="ts">
@@ -26,7 +24,6 @@ export default {
     computed: {
         ...mapState(["today", "flowerData"]),
     },
-    methods: {},
     mounted(): void {
         this.$store.commit(INITIALIZE_DATE);
         this.$store.dispatch(GET_FLOWER_DATA, this.$store.state.today.dateNo);
@@ -37,7 +34,8 @@ export default {
 <style lang="scss">
 .todayContainer {
     @include container(900, 12);
-    padding: 36px 0 32px;
+    text-align: center;
+    padding: 44px 30px 32px;
     .imgWrapper {
         margin: 16px auto 20px;
         width: 405px;
--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,22 @@ import axios from "axios";
 Vue.use(Vuex);
 
 // 윤년인지 검사
-const month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30];
+const monthArr = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30];
+
+const calculateDateNo = (month: number, day: number): number => {
+    return monthArr.slice(0, month + 1).reduce((a, b) => a + b, 0) + day;
+};
+
+const breakLine = (str: string): string => {
+    return (str + " ")
+        .split(/\.[ ]+/g)
+        .filter((elem) => elem !== "")
+        .join(".<br>");
+};
+
+const upperFirstChar = (str: string): string => {
+    return str[0].toUpperCase() + str.slice(1);
+};
 
 export const INITIALIZE_DATE = "INITIALIZE_DATE";
 export const SET_FLOWER = "SET_FLOWER";
@@ -19,22 +34,27 @@ export default new Vuex.Store({
             dateNo: 0,
         },
         flowerData: {
-            flowerName: "",
-            flowerLanguage: "",
-            flowerImg: [],
+            month: 0,
+            day: 0,
+            korName: "",
+            engName: "",
+            sctName: "",
+            language: "",
+            imgArray: [],
+            content: "",
+            use: "",
+            grow: "",
+            type: "",
         },
     },
     mutations: {
         // 페이지를 열 때마다 오늘의 날짜를 얻어와서 초기화
         [INITIALIZE_DATE](state) {
             const date = new Date();
-            const days =
-                month.slice(0, date.getMonth() + 1).reduce((a, b) => a + b, 0) +
-                date.getDate();
             state.today = {
                 month: date.getMonth() + 1,
                 day: date.getDate(),
-                dateNo: days,
+                dateNo: calculateDateNo(date.getMonth(), date.getDate()),
             };
         },
         [SET_FLOWER](state, data) {
@@ -54,21 +74,59 @@ export default new Vuex.Store({
                     data,
                     "text/xml"
                 );
-                console.log(result);
-                const flowerImg = [
-                    result.getElementsByTagName("imgUrl1")[0].textContent,
-                    result.getElementsByTagName("imgUrl2")[0].textContent,
-                    result.getElementsByTagName("imgUrl3")[0].textContent,
-                ];
-
-                const flowerName =
+                const month =
+                    result.getElementsByTagName("fMonth")[0].textContent;
+                const day = result.getElementsByTagName("fDay")[0].textContent;
+                const korName =
                     result.getElementsByTagName("flowNm")[0].textContent;
-                const flowerLanguage =
-                    result.getElementsByTagName("flowLang")[0].textContent;
+                const engName = upperFirstChar(
+                    result.getElementsByTagName("fEngNm")[0]
+                        .textContent as string
+                );
+                const sctName = upperFirstChar(
+                    result.getElementsByTagName("fSctNm")[0]
+                        .textContent as string
+                );
+                const language = result
+                    .getElementsByTagName("flowLang")[0]
+                    .textContent?.replace(".", "");
+                const imgArray = [];
+                for (let i = 0; i < 3; i++) {
+                    imgArray.push(
+                        result.getElementsByTagName(`imgUrl${i + 1}`)[0]
+                            .textContent
+                    );
+                }
+                const content = (
+                    result.getElementsByTagName("fContent")[0].textContent + " "
+                )
+                    .split(/\.[ ]+/g)
+                    .map((elem) => elem.replace(/<[/]*p>|\r\n/g, ""))
+                    .filter((elem) => elem !== "")
+                    .slice(0, 3);
+                const use = breakLine(
+                    result.getElementsByTagName("fUse")[0].textContent as string
+                );
+                const grow = breakLine(
+                    result.getElementsByTagName("fGrow")[0]
+                        .textContent as string
+                );
+                const type = breakLine(
+                    result.getElementsByTagName("fType")[0]
+                        .textContent as string
+                );
                 commit(SET_FLOWER, {
-                    flowerName,
-                    flowerLanguage,
-                    flowerImg,
+                    month,
+                    day,
+                    korName,
+                    engName,
+                    sctName,
+                    language,
+                    imgArray,
+                    content,
+                    use,
+                    grow,
+                    type,
                 });
                 return;
             } catch (error) {
