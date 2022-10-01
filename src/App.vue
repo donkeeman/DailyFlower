b23985399fd4de29921c425234e7ac8e008f885a
donkeeman <doonkeeemaan@gmail.com>
2022-10-01 18:28:38 +0900

    Feat: 라우터 경로 받아오는 함수 추가 및 경로에 따라 컴포넌트 보이는 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
     <div id="app">
         <Header></Header>
-        <TodayFlower></TodayFlower>
+        <main class="main">
+            <template v-if="getPath === ''">
+                <TodayFlower></TodayFlower>
+            </template>
+            <template v-else-if="getPath === 'info'">
+                <FlowerInfo></FlowerInfo>
+            </template>
+        </main>
+
+        <!-- 검색, 검색 결과 페이지 추가 -->
     </div>
 </template>
 
 <script lang="ts">
 import Header from "@/components/Header.vue";
 import TodayFlower from "./components/TodayFlower.vue";
+import FlowerInfo from "./components/FlowerInfo.vue";
 export default {
     components: {
         Header,
         TodayFlower,
+        FlowerInfo,
+    },
+    computed: {
+        getPath(): string {
+            return this.$route.path.split("/")[1];
+        },
     },
 };
 </script>
